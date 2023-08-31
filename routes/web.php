1737dac55bb3b0f7ae4e9160a663ae758f0f912b
DarioM <manghina.dario@gmail.com>
2023-08-31 07:39:50 +0200

    pre-stripe

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,23 +8,13 @@ use App\Http\Controllers\CustomerController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('/products/all', [ProductController::class, 'all']);
-Route::get('/product/category/{id}', [ProductController::class, 'findByCategory']);
-Route::get('/product/best', [ProductController::class, 'best']);
-Route::get('/product/{id}', [ProductController::class, 'findById']);
-Route::get('/product/{id}/img/{resolution}', [ProductController::class, 'findImgById']);
-Route::get('/product/{id}/img', [ProductController::class, 'findImgById']);
-Route::post('/orders/checkout', [OrderController::class, 'checkout']);
-Route::put('/customers/create', [CustomerController::class, 'create']);
-Route::get('/token', function () {
-    return csrf_token(); 
-});
+require __DIR__.'/auth.php';
