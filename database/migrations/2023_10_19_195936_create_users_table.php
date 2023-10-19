b6478bf098b1dd566115b9d65570a131e770f252
dreammaker0717 <tigerhunter0717@gmail.com>
2023-10-19 16:03:20 -0400

    Update : user table

---
+++ database/migrations/2023_10_19_195936_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('role')->default('CUSTOMER');
+            $table->string('coupon')->nullable();            
+            $table->decimal('percentage')->default(0);
+            $table->decimal('referral')->default(0);
+            $table->string('stripe_id')->nullable();
+            $table->string('card_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
